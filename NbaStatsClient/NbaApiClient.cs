d8c63a141bc0ac2f56b3439ccbc189d6b157654a
ethanbrown3 <25070760+ethanbrown3@users.noreply.github.com>
2019-07-27 10:45:36 -0600

    ApiClient class created

---
+++ NbaStatsClient/NbaApiClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace NbaStatsClient
+{
+    public class NbaApiClient
+    {
+        public static HttpClient ApiClient { get; set; }
+
+        public static void InitializeClient(string baseUrl="https://api.sportradar.us")
+        {
+            ApiClient = new HttpClient
+            {
+                BaseAddress = new Uri(baseUrl)
+            };
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
