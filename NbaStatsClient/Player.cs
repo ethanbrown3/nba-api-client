c208980b144370a3022c8fd1d14aea31ab770aed
ethanbrown-desktop <25070760+ethanbrown3@users.noreply.github.com>
2019-07-28 16:32:32 -0600

    Player model added

---
+++ NbaStatsClient/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NbaStatsClient
+{
+    public class Player
+    {
+        public string Id { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string Primary_Position { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}\n" +
+                 $"FirstName: {First_Name}\n" +
+                 $"LastName: {Last_Name}\n" +
+                 $"Position: {Primary_Position}\n";
+        }
+    }
+}
