46c7f948fdeb8242f473cd8a8417ff04c5f590ad
ethanbrown-desktop <25070760+ethanbrown3@users.noreply.github.com>
2019-07-28 16:45:53 -0600

    create NbaTeamRepo

---
+++ NbaStatsClient/NbaTeamRepo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NbaStatsClient
+{
+    public class NbaTeamRepo
+    {
+        private Dictionary<string, NbaTeam> NbaTeams { get; set; }
+
+        public NbaTeamRepo(List<NbaTeam> nbaTeams)
+        {
+            foreach (NbaTeam team in nbaTeams)
+            {
+                NbaTeams.Add(team.Id, team);
+            }
+        }
+    }
+}
