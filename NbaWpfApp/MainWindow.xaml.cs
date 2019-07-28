58bbb70ae887b6e8f620056d8543f2fd3cc38619
ethanbrown-desktop <25070760+ethanbrown3@users.noreply.github.com>
2019-07-28 16:33:55 -0600

    added text boxes and test get request methods

--- NbaWpfApp/MainWindow.xaml.cs
+++ NbaWpfApp/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using NbaStatsClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,11 +26,68 @@ namespace NbaWpfApp
         public MainWindow()
         {
             InitializeComponent();
+            NbaApiClient.InitializeClient();
         }
 
-        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        private async void SubmitButton_ClickAsync(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                var url = urlTextBox.Text;
+                var response = await LoadNbaTeam(url);
+                outputBox.AppendText(response.ToString());
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name, MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
 
+        public static async Task<string> LoadString(string url)
+        {
+            using (HttpResponseMessage response = await NbaApiClient.ApiClient.GetAsync(url))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseString = await response.Content.ReadAsStringAsync();
+
+                    return responseString;
+                }
+                else
+                {
+                    var ex = new Exception(response.ReasonPhrase);
+                    throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+                }
+            }
+        }
+
+        public static async Task<NbaTeam> LoadNbaTeam(string url)
+        {
+            using (HttpResponseMessage response = await NbaApiClient.ApiClient.GetAsync(url))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    NbaTeam nbaTeam = await response.Content.ReadAsAsync<NbaTeam>();
+
+                    return nbaTeam;
+                }
+                else
+                {
+                    var ex = new Exception(response.ReasonPhrase);
+                    throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+                }
+            }
+        }
+        private void HandleError(string sClass, string sMethod, string sMessage)
+        {
+            try
+            {
+                MessageBox.Show(sClass + "." + sMethod + " ->" + sMessage);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("HandleError Exception: " + ex.Message);
+            }
         }
     }
 }
