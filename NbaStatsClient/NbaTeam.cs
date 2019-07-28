79131e63c93c2c7d947c2f20ce2995a4aaa021e7
ethanbrown-desktop <25070760+ethanbrown3@users.noreply.github.com>
2019-07-28 16:32:24 -0600

    NbaTeam Model added

---
+++ NbaStatsClient/NbaTeam.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NbaStatsClient
+{
+    public class NbaTeam
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Market { get; set; }
+        public string Alias { get; set; }
+        public string Founded { get; set; }
+
+        public List<Player> Players { get; set; }
+
+        public override string ToString()
+        {
+            
+            var playerList = new List<string>();
+            foreach (Player player in Players)
+            {
+                playerList.Add(player.ToString());
+            }
+            string toString = $"ID: {Id}\n" +
+                $"Name: {Name}\n" +
+                $"Market: {Market}\n" +
+                $"Alias: {Alias}\n" +
+                $"Founded: {Founded}\n" +
+                $"Players: {string.Join("\n", playerList)}";
+
+            return toString;
+        }
+    }
+}
